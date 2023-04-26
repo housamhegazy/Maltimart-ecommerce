194c4803f76e6e5c146b989ee4e417622d0fc35f
Housam Hegazy <geohousam@gmail.com>
2023-04-26 09:09:12 +0000

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,40 @@
 import "./App.css";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import Roote from "./Routers/Roote";
+import Home from "./Pages/Home";
+import Cart from "./Pages/Cart";
+import ProductDetails from "./Pages/ProductDetails";
+import Login from "./Pages/Login";
+import SignUp from "./Pages/SignUp";
+import Shope from "./Pages/Shope";
+import Checkout from "./Pages/Checkout";
+import Error404 from "./Pages/Error404";
 
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Roote />}>
+      <Route index element={<Home />} />
+      <Route path="/shope" element={<Shope />} />
+      <Route path="/cart" element={<Cart />} />
+      <Route path="/productDetails" element={<ProductDetails />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<SignUp />} />
+      <Route path="/checkout" element={<Checkout />} />
+      <Route path="*" element={<Error404 />} />
+      
+    </Route>
+  )
+);
 function App() {
-  return <h1>hello world</h1>;
+  return(
+    <RouterProvider router={router} />
+  )
+  
 }
 
 export default App;
