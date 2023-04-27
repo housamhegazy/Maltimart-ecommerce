fca8a67901823f99be8509ae00a6494ea44242a0
Housam Hegazy <geohousam@gmail.com>
2023-04-27 11:08:11 +0000

    update

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import "remixicon/fonts/remixicon.css";
 import "bootstrap/dist/css/bootstrap.css";
 
+
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
