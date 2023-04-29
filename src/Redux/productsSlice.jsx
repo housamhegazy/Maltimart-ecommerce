fbc71ddb3c581718175d6d160304b32c6b6c58be
Housam Hegazy <geohousam@gmail.com>
2023-04-29 18:03:45 +0000

    update

---
+++ src/Redux/productsSlice.jsx
@@ -0,0 +1,23 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+  cartItems:[],
+  totalAmount:0,
+  totalQuantity:0
+}
+
+export const productsSlice = createSlice({
+  name: 'cart',
+  initialState,
+  reducers: {
+    AddItem: (state, action) => {
+      
+    },
+    
+  },
+})
+
+// Action creators are generated for each case reducer function
+export const {  } = productsSlice.actions
+
+export default productsSlice.reducer
--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "remixicon/fonts/remixicon.css";
 import "bootstrap/dist/css/bootstrap.css";
-
+import { store } from "./Redux/Store";
+import { Provider } from "react-redux";
 
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
