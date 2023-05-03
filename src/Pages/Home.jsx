badc0256f0b41e251bf263c3d62e896c3f127026
housamhegazy <geohousam@gmail.com>
2023-05-04 02:40:14 +0300

    update

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -7,11 +7,16 @@ import { Link } from 'react-router-dom'
 import { motion } from 'framer-motion'
 import ServicesComp from '../services/Servicess'
 import ProductList from 'Components/Ul/ProductList'
-import products from 'assets/data/products'
+// import products from 'assets/data/products'
 import counterImg from '../assets/images/counter-timer-img.png'
 import Clock from 'Components/Ul/Clock'
+import { useCollection } from "react-firebase-hooks/firestore";
+import { collection } from "firebase/firestore";
+import { db } from "../firebase/config";
 
 export default function Home() {
+  const [value, loading, error] = useCollection(collection(db, "products"));
+
 const [trendingProducts,settrendingProducts] = useState([])
 const [bestSalesProducts,setbestSalesProducts] = useState([])
 const [mobileproducts,setmobileproducts] = useState([])
@@ -19,105 +24,110 @@ const [wirelessProducts,setwirelessProducts] = useState([])
 const [popularProducts,setpopularProducts] = useState([])
 
 useEffect(()=>{
-  const filteredtrendingProducts = products.filter((item)=>{return item.category === "chair"});
-  const filteredbestSalesProducts = products.filter((item)=>{return item.category === "sofa"});
-  const filteredmobileproducts = products.filter((item)=>{return item.category === "mobile"});
-  const filteredwirelessProducts = products.filter((item)=>{return item.category === "wireless"});
-  const filteredpopularProducts = products.filter((item)=>{return item.category === "watch"});
+  if(value){
+    const filteredtrendingProducts = value.docs.filter((item)=>{return item.data().category === "chair"});
+  const filteredbestSalesProducts = value.docs.filter((item)=>{return item.data().category === "sofa"});
+  const filteredmobileproducts = value.docs.filter((item)=>{return item.data().category === "mobile"});
+  const filteredwirelessProducts = value.docs.filter((item)=>{return item.data().category === "wireless"});
+  const filteredpopularProducts = value.docs.filter((item)=>{return item.data().category === "watch"});
   settrendingProducts(filteredtrendingProducts)
   setbestSalesProducts(filteredbestSalesProducts)
   setmobileproducts(filteredmobileproducts)
   setwirelessProducts(filteredwirelessProducts)
   setpopularProducts(filteredpopularProducts)
-},[])
+  }
+  
+},[value])
   const Year = new Date().getFullYear()
-  return (
-    <Helmet title={"Home"}>
-      <section className='hero__section'>
-        <Container>
-          <Row>
-            <Col lg='6' md='6'>
-              <div className="hero__content">
-                <p className="hero__subtitle">
-                  trending produc in {Year}
-                </p>
-                <h2>Make your Interior More Minimalistic and Modern </h2>
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita modi earum dolores quasi aliquid id, dolore ad nulla est ipsum?</p>
-                <motion.button whileTap={{scale:1.2}} className="buy__btn">
-                  <Link to={'shope'}>SHOPE NOW</Link>
+  if(value){
+    return (
+      <Helmet title={"Home"}>
+        <section className='hero__section'>
+          <Container>
+            <Row>
+              <Col lg='6' md='6'>
+                <div className="hero__content">
+                  <p className="hero__subtitle">
+                    trending produc in {Year}
+                  </p>
+                  <h2>Make your Interior More Minimalistic and Modern </h2>
+                  <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita modi earum dolores quasi aliquid id, dolore ad nulla est ipsum?</p>
+                  <motion.button whileTap={{scale:1.2}} className="buy__btn">
+                    <Link to={'shope'}>SHOPE NOW</Link>
+                  </motion.button>
+                </div>
+              </Col>
+              <Col lg='6' md='6'>
+                <div className="hero__img">
+                  <img src={HeroImage} alt="heroimage" />
+                </div>
+              </Col>
+            </Row>
+          </Container>
+        </section>
+        <ServicesComp/>
+        <section className="trending__products">
+          <Container>
+            <Row>
+              <Col lg='12' className='text-center'>
+                <h2 className='section__title'>Trending Products</h2>
+              </Col>
+              <ProductList data={trendingProducts}/>
+            </Row>
+          </Container>
+        </section>
+        <section className="best__sales">
+          <Container>
+            <Row>
+              <Col lg='12' className='text-center'>
+                <h2>Best Sales</h2>
+              </Col>
+              <ProductList data={bestSalesProducts}/>
+            </Row>
+          </Container>
+        </section>
+        <section className="timer-count">
+          <Container>
+            <Row>
+              <Col lg='6' md='12'>
+                <div className="clock__top-content ">
+                  <h4 className='text-white fs-6 mb-2'>Limited offer</h4>
+                  <h3 className='text-white fs-5 mb-3'>Quality Armchair</h3>
+                </div>
+                
+                <Clock/>
+                <motion.button whileTap={{scale:1.1}} className="buy__btn store__btn">
+                  <Link to={'/shope'}>Visit Store</Link>
                 </motion.button>
-              </div>
-            </Col>
-            <Col lg='6' md='6'>
-              <div className="hero__img">
-                <img src={HeroImage} alt="heroimage" />
-              </div>
-            </Col>
-          </Row>
-        </Container>
-      </section>
-      <ServicesComp/>
-      <section className="trending__products">
-        <Container>
-          <Row>
-            <Col lg='12' className='text-center'>
-              <h2 className='section__title'>Trending Products</h2>
-            </Col>
-            <ProductList data={trendingProducts}/>
-          </Row>
-        </Container>
-      </section>
-      <section className="best__sales">
-        <Container>
-          <Row>
-            <Col lg='12' className='text-center'>
-              <h2>Best Sales</h2>
-            </Col>
-            <ProductList data={bestSalesProducts}/>
-          </Row>
-        </Container>
-      </section>
-      <section className="timer-count">
-        <Container>
-          <Row>
-            <Col lg='6' md='12'>
-              <div className="clock__top-content ">
-                <h4 className='text-white fs-6 mb-2'>Limited offer</h4>
-                <h3 className='text-white fs-5 mb-3'>Quality Armchair</h3>
-              </div>
-              
-              <Clock/>
-              <motion.button whileTap={{scale:1.1}} className="buy__btn store__btn">
-                <Link to={'/shope'}>Visit Store</Link>
-              </motion.button>
-            </Col>
-            <Col lg='6' md='12' className='text-end counter__img'>
-              <img src={counterImg} alt='counterimg'/>
-            </Col>
-          </Row>
-        </Container>
-      </section>
-      <section className="new-arrivals">
-        <Container>
-          <Row>
-            <Col lg='12' className='text-center mb-5'>
-                <h2>New Arrivals</h2>
-            </Col>
-            <ProductList data={mobileproducts}/>
-            <ProductList data={wirelessProducts}/>
-          </Row>
-        </Container>
-      </section>
-      <section className="popular-category">
-        <Container>
-          <Row>
-            <Col lg='12' className='text-center mb-5'>
-                <h2>Popular In Category</h2>
-            </Col>
-            <ProductList data={popularProducts}/>
-          </Row>
-        </Container>
-      </section>
-    </Helmet>
-  )
+              </Col>
+              <Col lg='6' md='12' className='text-end counter__img'>
+                <img src={counterImg} alt='counterimg'/>
+              </Col>
+            </Row>
+          </Container>
+        </section>
+        <section className="new-arrivals">
+          <Container>
+            <Row>
+              <Col lg='12' className='text-center mb-5'>
+                  <h2>New Arrivals</h2>
+              </Col>
+              <ProductList data={mobileproducts}/>
+              <ProductList data={wirelessProducts}/>
+            </Row>
+          </Container>
+        </section>
+        <section className="popular-category">
+          <Container>
+            <Row>
+              <Col lg='12' className='text-center mb-5'>
+                  <h2>Popular In Category</h2>
+              </Col>
+              <ProductList data={popularProducts}/>
+            </Row>
+          </Container>
+        </section>
+      </Helmet>
+    )
+  }
 }
--- src/admin/Dashboard.jsx
+++ src/admin/Dashboard.jsx
@@ -1,7 +1,47 @@
 import React from 'react'
-
+import '../styles/dashboard.css'
+import {Container,Row,Col} from 'reactstrap'
+import { useCollection } from "react-firebase-hooks/firestore";
+import { collection } from "firebase/firestore";
+import { db } from "../firebase/config";
 export default function Dashboard() {
+  const [value, loading, error] = useCollection(collection(db, "products"));
+  const [value2,loading2, error2] = useCollection(collection(db, "users"));
+if(value2){
+  console.log(value2);
+}
   return (
-    <div>Dashboard</div>
+    <>
+      <section>
+        <Container>
+          <Row>
+            <Col lg='3'>
+              <div className="revenue-box">
+                <h5>Total Sale</h5>
+                <span>$7899</span>
+              </div>
+            </Col>
+            <Col lg='3'>
+              <div className="order-box">
+                <h5>Orders</h5>
+                <span>766</span>
+              </div>
+            </Col>
+            <Col lg='3'>
+              <div className="products-box">
+                <h5>total Products</h5>
+                <span>{value && value.docs.length}</span>
+              </div>
+            </Col>
+            <Col lg='3'>
+            <div className="users-box">
+                <h5>total users</h5>
+                <span>{value2 && value2.docs.length}</span>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    </>
   )
 }
